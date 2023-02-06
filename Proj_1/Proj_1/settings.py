32c0160850ef8653e94bb810ea9a1bf73923695d
Harsh <Sharmaharsh941@gmail.com>
2023-02-06 23:47:53 +0530

    Changing Secret key

--- Proj_1/Proj_1/settings.py
+++ Proj_1/Proj_1/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njo*$2$l4b&rvevs$0_lhp4iecv(#h&eejud8i4+ur#-i30kon'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
